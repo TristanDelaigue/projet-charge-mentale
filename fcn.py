16335f0ba6e5d44f4386e036204dbd3e0969d92c
redzebra42 <delaigletristan42@gmail.com>
2025-11-21 17:34:02 +0100

    Fix convolutionnal layers in fcn

--- fcn.py
+++ fcn.py
@@ -5,10 +5,11 @@ import torch.optim as optim
 
 class FCN(nn.Module):
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, input_size, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.conv1 = nn.Conv1d(64, 64)
+        self.conv1 = nn.Conv1d(input_size, 64)
+        self.conv2 = nn.Conv1d(64, 64)
         self.gap = nn.AvgPool1d()
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax()
@@ -17,26 +18,26 @@ class FCN(nn.Module):
         
         # ecg layer
         x_ecg = self.relu(self.conv1(x_ecg))
-        x_ecg = self.relu(self.conv1(x_ecg))
-        x_ecg = self.relu(self.conv1(x_ecg))
+        x_ecg = self.relu(self.conv2(x_ecg))
+        x_ecg = self.relu(self.conv2(x_ecg))
         x_ecg = self.gap(x_ecg)
         
         # gsr layer
         x_gsr = self.relu(self.conv1(x_gsr))
-        x_gsr = self.relu(self.conv1(x_gsr))
-        x_gsr = self.relu(self.conv1(x_gsr))
+        x_gsr = self.relu(self.conv2(x_gsr))
+        x_gsr = self.relu(self.conv2(x_gsr))
         x_gsr = self.gap(x_gsr)
         
-        #inf ppg layer
+        # inf ppg layer
         x_inf_ppg = self.relu(self.conv1(x_inf_ppg))
-        x_inf_ppg = self.relu(self.conv1(x_inf_ppg))
-        x_inf_ppg = self.relu(self.conv1(x_inf_ppg))
+        x_inf_ppg = self.relu(self.conv2(x_inf_ppg))
+        x_inf_ppg = self.relu(self.conv2(x_inf_ppg))
         x_inf_ppg = self.gap(x_inf_ppg)
         
         # pix ppg layer
         x_pix_ppg = self.relu(self.conv1(x_pix_ppg))
-        x_pix_ppg = self.relu(self.conv1(x_pix_ppg))
-        x_pix_ppg = self.relu(self.conv1(x_pix_ppg))
+        x_pix_ppg = self.relu(self.conv2(x_pix_ppg))
+        x_pix_ppg = self.relu(self.conv2(x_pix_ppg))
         x_pix_ppg = self.gap(x_pix_ppg)
         
         # concatenate all layers
